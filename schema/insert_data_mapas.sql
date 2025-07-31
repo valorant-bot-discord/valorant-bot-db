2f38b1016864277dc8fbe8ef0552c4ad5ecb5fce
EmilyCV <emily_cvasconcelos@hotmail.com>
2025-07-30 22:02:48 -0300

    feat: adiciona scripts sql e gitignore

---
+++ schema/insert_data_mapas.sql
@@ -0,0 +1,13 @@
+INSERT INTO mapas (id_mapa, nome, quantidade_bombs)
+VALUES (1, 'Ascent', 2),
+       (2, 'Haven', 3),
+       (3, 'Split', 2),
+       (4, 'Bind', 2),
+       (5, 'Icebox', 2),
+       (6, 'Breeze', 2),
+       (7, 'Fracture', 2),
+       (8, 'Pearl', 2),
+       (9, 'Lotus', 3),
+       (10, 'Sunset', 2),
+       (11, 'Abyss', 2),
+       (12, 'Corrode', 2);
